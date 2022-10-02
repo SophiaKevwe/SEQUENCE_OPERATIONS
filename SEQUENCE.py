33762fe50b3c984ce2a588217b9067f516e208c1
Sophia <oghenekes73@gmail.com>
2022-10-02 23:02:11 +0100

    BASIC SEQUENCE OPERATIONS

---
+++ SEQUENCE.py
@@ -0,0 +1,23 @@
+a = [[1, 2, 3], 2, 5, ["CAT", "DOG", "SHEEP", "5 BALLS"]]
+print(a)
+print(a[3][3][0])
+b = "banana"
+b[0]=b[0].upper()
+print(b)
+print(b.split("n"))
+print(list(b))
+c =[1,2,3,4,5,6,7,8,9,10]
+c[3]=14
+print(c)
+print(c[0:5])
+print(c[2:10])
+print(c[2:])
+print(b[2:])
+
+b=list(b)
+b[2] = "t"
+b = ("".join(b))
+print(b)
+
+
+
